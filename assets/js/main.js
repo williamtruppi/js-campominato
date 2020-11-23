1433e2af109b361b663a511d8a6d7d59e3e26a26
William Truppi <williamt@hotmail.it>
2020-11-23 21:53:22 +0100

    L’utente non può inserire più volte lo stesso numero.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,37 @@
 // Il computer deve generare 16 numeri casuali tra 1 e 100.
-
-var cpuNumber = 0
-var cpuNumbersArray = [];
-
-
-for (var i = 0; i < 16 && cpuNumber != cpuNumbersArray[i]; i++){
-  cpuNumber = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
-  cpuNumbersArray.push(cpuNumber);
-}
-
-console.log(cpuNumbersArray);
-
 // I numeri non possono essere duplicati
 
-for (var j = 0; j < cpuNumbersArray.length / 2; j++){
-  for (var y = cpuNumbersArray.length / 2; y < cpuNumbersArray.length; y++){
-    if (cpuNumbersArray[j] == cpuNumbersArray[y]){
-      cpuNumbersArray[j] = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
-    }
+var cpuNumber = 0;
+var cpuNumbersArray = [];
+
+while (cpuNumbersArray.length < 16) {
+  var cpuNumber = randomNumber(1, 100); //invoco la funziona randomNumber
+  if (!(cpuNumbersArray.includes(cpuNumber))) { 
+    cpuNumbersArray.push(cpuNumber);
   }
 }
 
 console.log(cpuNumbersArray);
 
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+// L’utente non può inserire più volte lo stesso numero.
 
 var userNumbersArray = [];
-var x = 0;
-var trovato = false;
+var userNumber;
 
-do{
-  userNumber = Number(prompt("Inserisci un numero tra 1 (compreso) e 100 (compreso)"));
-  userNumbersArray.push(userNumber);
-  x++;
-} while (userNumber < 1 || userNumber > 100 || isNaN(userNumber) || userNumbersArray.length != 10)
+while (userNumbersArray.length < 5) {
+  var userNumber = Number(prompt("Inserisci un numero da 1 (compreso) a 100 (compreso"));
+  if (!(userNumbersArray.includes(userNumber))) { 
+    userNumbersArray.push(userNumber);
+  } else {
+    alert("Hai già inserito questo numero - Per favore, riprova");
+  }
+}
 
 console.log(userNumbersArray);
+
+
+//FUNZIONI
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min); //incluso sia il minimo che il massimo
+}
