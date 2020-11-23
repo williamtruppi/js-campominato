ba8021f0d7310d091291f7c47d7a9b7cfe99c55b
William Truppi <williamt@hotmail.it>
2020-11-23 15:07:52 +0100

    Eliminazione duplicati array numeri casuali

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,23 @@
 var cpuNumber = 0
 var cpuNumbersArray = [];
 
-for (var i = 0; i < 16; i++){
+
+for (var i = 0; i < 16 && cpuNumber != cpuNumbersArray[i]; i++){
   cpuNumber = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
   cpuNumbersArray.push(cpuNumber);
 }
+
+console.log(cpuNumbersArray);
+
+// I numeri non possono essere duplicati
+
+for (var j = 0; j < cpuNumbersArray.length / 2; j++){
+  for (var y = cpuNumbersArray.length / 2; y < cpuNumbersArray.length; y++){
+    if (cpuNumbersArray[j] == cpuNumbersArray[y]){
+      cpuNumbersArray[j] = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+    }
+  }
+}
+
+
 console.log(cpuNumbersArray);
